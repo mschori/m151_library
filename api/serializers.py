3253487907c4fbf1eb3a262d708b51da713b088f
Michael Schori <michael.schori@gmx.ch>
2020-10-31 11:49:07 +0100

    Fixed nested model-infos.

--- api/serializers.py
+++ api/serializers.py
@@ -6,14 +6,33 @@ class BookSerializer(serializers.ModelSerializer):
     author_pks = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), source='authors', write_only=True,
                                                     many=True, label='Authors')
 
+    def __init__(self, *args, **kwargs):
+        # Get additional parameters from constructor
+        depth = kwargs.pop('depth', None)
+        fields = kwargs.pop('fields', None)
+
+        # Add author_pks to fields if field is not None from constructor
+        fields.append('author_pks') if fields is not None else None
+
+        # Set Meta-Tags
+        self.Meta.depth = depth if depth is not None else 1
+        self.Meta.fields = fields if fields is not None else '__all__'
+
+        # Call super-constructor
+        super(BookSerializer, self).__init__()
+
     class Meta:
         model = Book
-        fields = ('id', 'isbn', 'title', 'pages', 'is_borrowed', 'authors', 'author_pks')
-        depth = 1
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    book_list = BookSerializer(many=True, read_only=True)
+    # Create field-list for nested-books
+    book_fields = ['id', 'isbn', 'title', 'pages', 'is_borrowed']
+
+    # Create book-list for nested books with options
+    book_list = BookSerializer(many=True, read_only=True, depth=0, fields=book_fields)
+
+    # Change image-options to allow post/put/patch without an image
     image = serializers.ImageField(allow_null=True)
 
     class Meta:
