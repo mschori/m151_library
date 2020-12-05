afb1f3b4945162f2972983c85094c44d3306038a
Michael Schori <michael.schori@gmx.ch>
2020-12-05 08:43:47 +0100

    Added heroku-settings

--- m151_library/settings.py
+++ m151_library/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+# Heroku Settings
+django_heroku.settings(locals())
